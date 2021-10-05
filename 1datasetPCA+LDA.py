b0eabb1bbae3772fd3fcd48aa6aa2d72e9608b0f
athoikgeo <72350919+athoikgeo@users.noreply.github.com>
2021-10-05 13:34:54 +0300

    Update 1datasetPCA+LDA.py

--- 1datasetPCA+LDA.py
+++ 1datasetPCA+LDA.py
@@ -21,7 +21,6 @@ X_final = np.array(X_final)
 
 X_train, X_test, y_train, y_test = train_test_split(X_final, y, test_size=0.3, random_state=42)
 
-#kanonikopoiisi twn train kai twn test dedomenwn
 
 scaler = StandardScaler()
 scaler.fit(X_train)
@@ -35,7 +34,6 @@ def my_scorer(estimator, X_train, y=None):
     X_preimage = estimator.inverse_transform(X_reduced)
     return -1 * mean_squared_error(X_train, X_preimage)
 
-#upologismos tou kaluterou sindiasmou synartisis pirina kai parametrou gamma
 
 param_grid = {"gamma": [0.0001, 0.001, 0.01, 0.1],"kernel": ["rbf", "linear", "poly"]}
 kpca = KernelPCA(fit_inverse_transform=True, n_jobs=-1)
@@ -43,7 +41,6 @@ grid_search = GridSearchCV(kpca, param_grid=param_grid , cv=3, scoring= my_score
 grid_search.fit(X_train)
 print(grid_search.best_params_)
 
-#upologismos tis kaliteris ptwsis diastasis me KPCA
 
 #steps = [ ('KPCA', KernelPCA(kernel='linear')), ('KNN',KNeighborsClassifier(n_neighbors=1, metric='euclidean'))]
 steps = [ ('KPCA', KernelPCA(kernel='linear')), ('KNC',NearestCentroid( metric='euclidean'))]
@@ -56,7 +53,6 @@ print('\n all the parameters:', grid.cv_results_)
 print('Score on training_set: ', grid.score(X_train, y_train))
 print('Score on test_set: ', grid.score(X_test, y_test))
 
-# Plot tis meiwsis diastasts me to mean test score gia PCA
 
 aa = parameters['KPCA__n_components']
 bb = grid.cv_results_['mean_test_score']
@@ -66,7 +62,6 @@ plt.xlabel('Number of KPCA Components', fontsize = 10)
 plt.ylabel('Mean Test Score', fontsize = 10)
 plt.show()
 
-# Ypologismos tis kaliteris meiwsi diastasis me LDA
 
 #steps = [ ('KPCA', KernelPCA(kernel='linear',gamma=0.0001,n_components=81)),('LDA', LDA()),('KNN',KNeighborsClassifier(n_neighbors=1, metric='euclidean'))]
 steps = [ ('KPCA', KernelPCA(kernel='linear',gamma=0.0001,n_components=67)),('LDA', LDA()),('KNC',NearestCentroid( metric='euclidean'))]
@@ -79,7 +74,6 @@ print('\n all the parameters:', grid.cv_results_)
 print('Score on training_set: ', grid.score(X_train, y_train))
 print('Score on test_set: ', grid.score(X_test, y_test))
 
-# Plot tis meiwsis diastasts me to mean test score gia PCA
 
 aa = parameters['LDA__n_components']
 bb = grid.cv_results_['mean_test_score']
