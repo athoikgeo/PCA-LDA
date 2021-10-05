ebd0c3fb83a34e8a9055fc401ec2edc6ef8b121d
athoikgeo <72350919+athoikgeo@users.noreply.github.com>
2021-10-05 13:36:21 +0300

    Update 2odatasetPCA+LDA.py

--- 2odatasetPCA+LDA.py
+++ 2odatasetPCA+LDA.py
@@ -24,7 +24,6 @@ X_final = np.array(X_final)
 
 X_train, X_test, y_train, y_test = train_test_split(X_final, y, test_size=0.3, random_state=42)
 
-#kanonikopoiisi twn train kai twn test dedomenwn
 
 scaler = StandardScaler()
 scaler.fit(X_train)
@@ -38,7 +37,6 @@ def my_scorer(estimator, X_train, y=None):
     X_preimage = estimator.inverse_transform(X_reduced)
     return -1 * mean_squared_error(X_train, X_preimage)
 
-#upologismos tou kaluterou sindiasmou synartisis pirina kai parametrou gamma
 
 param_grid = [{
     "gamma": [0.0001, 0.001, 0.01, 0.1],
@@ -50,7 +48,6 @@ grid_search = GridSearchCV(kpca, param_grid, cv=3, scoring=my_scorer)
 grid_search.fit(X_train)
 print(grid_search.best_params_)
 
-#upologismos tis kaliteris ptwsis diastasis me KPCA
 
 #steps = [ ('KPCA', KernelPCA(kernel='linear')), ('KNC', NearestCentroid( metric='euclidean'))]
 steps = [ ('KPCA', KernelPCA(kernel='linear')), ('KNN',KNeighborsClassifier(n_neighbors=1, metric='euclidean'))]
@@ -63,7 +60,6 @@ print('\n all the parameters:', grid.cv_results_)
 print('Score on training_set: ', grid.score(X_train, y_train))
 print('Score on test_set: ', grid.score(X_test, y_test))
 
-# Plot tis meiwsis diastasts me to mean test score gia PCA
 
 aa = parameters['KPCA__n_components']
 bb = grid.cv_results_['mean_test_score']
@@ -73,7 +69,6 @@ plt.xlabel('Number of KPCA Components', fontsize = 10)
 plt.ylabel('Mean Test Score', fontsize = 10)
 plt.show()
 
-#upologismos tis kaliteris ptwsis diastasis me LDA
 
 #steps = [ ('KPCA', KernelPCA(kernel='linear',gamma=0.0001,n_components=60)),('LDA', LDA()),('KNC',NearestCentroid( metric='euclidean'))]
 steps = [ ('KPCA', KernelPCA(kernel='linear',gamma=0.0001,n_components=59)),('LDA', LDA()),('KNN',KNeighborsClassifier(n_neighbors=1, metric='euclidean'))]
@@ -86,7 +81,6 @@ print('\n all the parameters:', grid.cv_results_)
 print('Score on training_set: ', grid.score(X_train, y_train))
 print('Score on test_set: ', grid.score(X_test, y_test))
 
-# Plot tis meiwsis diastasts me to mean test score gia LDA
 
 aa = parameters['LDA__n_components']
 bb = grid.cv_results_['mean_test_score']
@@ -96,9 +90,6 @@ plt.xlabel('Number of LDA Components', fontsize = 10)
 plt.ylabel('Mean Test Score', fontsize = 10)
 plt.show()
 
-
-
-#grafiki parastasi twn dedomenwn me KPCA stis 2 diastaseis
 
 transformer = KernelPCA(n_components=2, kernel='linear')
 X_transformed = transformer.fit_transform(X_train)
@@ -119,7 +110,6 @@ colormap = [colordict[x] for x in y_train]
 plt.scatter(X_transformed[:,0],X_transformed[:,1], c = colormap)
 plt.show()
 
-#grafiki parastasi twn dedomenwn me kpca + lda stis 2 diastaseis
 
 transformer = KernelPCA(n_components=9, kernel='linear')
 y = transformer.fit_transform(X_train)
